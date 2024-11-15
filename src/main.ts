e51f9d86a597de7314be31bb73b40c5fa6af32c8
dylanbc1 <dyez1110@gmail.com>
2024-11-15 12:58:52 -0500

    feat: Correct CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // guarda el objeto SOLO con lo necesario
+      // asi en el JSON yo envie mas datos de los encesarios,
+      // solo obtiene los que yo tengo definidos en la clase de ese
+      // tipo de objeto
+      forbidNonWhitelisted: true, // me informa del error del envio
+      // de un dato o atributo no necesario y que no tengo definido
+    })
+    );
+
+    const corsOptions: CorsOptions = {
+      origin: '*', // Permite solicitudes desde este origen
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+      credentials: true, // Permitir el uso de cookies
+      allowedHeaders: 'Content-Type, Accept, Authorization', // Encabezados permitidos
+    };
+  
+    app.enableCors(corsOptions); // Habilitar CORS
+
+  await app.listen(8000);
 }
 bootstrap();
